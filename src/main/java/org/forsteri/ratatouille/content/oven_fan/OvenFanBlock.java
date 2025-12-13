ff86459cbd3df7c07e216aa0c6055c63a2825a91
thaumstrial <2738722282@qq.com>
2025-12-13 10:50:01 +0800

    fix: oven fan hasShaftTowards

--- src/main/java/org/forsteri/ratatouille/content/oven_fan/OvenFanBlock.java
+++ src/main/java/org/forsteri/ratatouille/content/oven_fan/OvenFanBlock.java
@@ -62,8 +62,7 @@ public class OvenFanBlock extends HorizontalKineticBlock implements ICogWheel, I
 
     @Override
     public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
-        return face == state.getValue(HORIZONTAL_FACING)
-                .getOpposite();
+        return false;
     }
 
     public void updateIndirectNeighbourShapes(BlockState stateIn, LevelAccessor worldIn, BlockPos pos, int flags, int count) {
