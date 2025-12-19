c4b1351af99b68b3439fb7f90ecb8e4f7c479e1d
Neve133 <hzh15914420771@gmail.com>
2025-12-19 10:13:40 +0900

    add: compost tower test recipes

--- src/main/java/org/forsteri/ratatouille/data/recipe/CRCompostingRecipeGen.java
+++ src/main/java/org/forsteri/ratatouille/data/recipe/CRCompostingRecipeGen.java
@@ -1,8 +1,14 @@
 package org.forsteri.ratatouille.data.recipe;
 
+import com.simibubi.create.AllFluids;
+import com.simibubi.create.AllItems;
 import com.simibubi.create.api.data.recipe.ProcessingRecipeGen;
 import com.simibubi.create.foundation.recipe.IRecipeTypeInfo;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.material.WaterFluid;
+import net.minecraftforge.common.Tags;
 import org.forsteri.ratatouille.Ratatouille;
 import org.forsteri.ratatouille.data.recipe.api.BakingRecipeGen;
 import org.forsteri.ratatouille.data.recipe.api.CompostingRecipeGen;
@@ -11,13 +17,29 @@ import org.forsteri.ratatouille.entry.CRItems;
 import org.forsteri.ratatouille.entry.CRRecipeTypes;
 
 public class CRCompostingRecipeGen extends CompostingRecipeGen {
-    GeneratedRecipe DEFAULT = this.create("composting", b -> b
+    GeneratedRecipe
+            DEFAULT = this.create("composting", b -> b
             .require(CRItems.COMPOST_MASS.get())
             .duration(200)
             //.output(CRItems.COMPOST_RESIDUE.get(), 1)
             .output(CRFluids.COMPOST_RESIDUE_FLUID.get(), 60)
             .output(CRFluids.COMPOST_TEA.get(), 30)
-            .output(CRFluids.BIO_GAS.get(), 10));
+            .output(CRFluids.BIO_GAS.get(), 10)),
+
+            DOUGH = this.create("more_dough", b -> b
+                    .require(AllItems.DOUGH.get())
+                    .require(AllItems.WHEAT_FLOUR.get())
+                    .require(Fluids.WATER, 100)
+                    .duration(200)
+                    .output(AllItems.DOUGH.get(),3)),
+
+            TEST1 = this.create("test1", b -> b
+                    .require(Fluids.WATER,50)
+                    .require(Tags.Fluids.MILK, 50)
+                    .require(Fluids.LAVA, 50)
+                    .duration(200)
+                    .output(AllFluids.HONEY.get(),150))
+    ;
 
     public CRCompostingRecipeGen(PackOutput generator) {
         super(generator, Ratatouille.MOD_ID);
