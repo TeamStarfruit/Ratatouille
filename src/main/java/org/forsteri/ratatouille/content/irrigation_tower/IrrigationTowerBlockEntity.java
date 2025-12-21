f11db7bd7c9e39f218f78c3bd28bf1d4f6a1ca9c
thaumstrial <2738722282@qq.com>
2025-12-21 13:35:18 +0800

    feat: irrigation tower tank capacity

--- src/main/java/org/forsteri/ratatouille/content/irrigation_tower/IrrigationTowerBlockEntity.java
+++ src/main/java/org/forsteri/ratatouille/content/irrigation_tower/IrrigationTowerBlockEntity.java
@@ -3,13 +3,16 @@ package org.forsteri.ratatouille.content.irrigation_tower;
 import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
 import com.simibubi.create.foundation.fluid.SmartFluidTank;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import org.forsteri.ratatouille.entry.CRFluids;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -27,6 +30,31 @@ public class IrrigationTowerBlockEntity extends FluidTankBlockEntity {
 
     public IrrigationTowerBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+    }
+
+    @Override
+    public void applyFluidTankSize(int blocks) {
+        tankInventory.setCapacity(blocks * 1000);
+        int overflow = tankInventory.getFluidAmount() - tankInventory.getCapacity();
+        if (overflow > 0)
+            tankInventory.drain(overflow, IFluidHandler.FluidAction.EXECUTE);
+        forceFluidLevelUpdate = true;
+    }
+
+    @Override
+    protected void read(CompoundTag compound, boolean clientPacket) {
+        super.read(compound, clientPacket);
+        if (isController()) {
+            tankInventory.setCapacity(getTotalTankSize() * 1000);
+            tankInventory.readFromNBT(compound.getCompound("TankContent"));
+            if (tankInventory.getSpace() < 0)
+                tankInventory.drain(-tankInventory.getSpace(), IFluidHandler.FluidAction.EXECUTE);
+        }
+    }
+
+    @Override
+    public int getTankSize(int tank) {
+        return 1000;
     }
 
     @Override
